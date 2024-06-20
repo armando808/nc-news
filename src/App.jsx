7c9fd3258bb450056637d6fc00988c28dfd19aa8
Arthur (armando808) <arthur.grimley@hotmail.com>
2024-06-20 15:51:26 +0100

    t8 post comments with optimistic rendering and error handling

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,20 @@ import ArticlesPage from "./Components/ArticlesPage"
 import IndividualArticle from "./Components/IndividualArticle"
 import HomePage from "./Components/HomePage"
 import NotFound from "./Components/NotFound"
+import { UserProvider } from "./UserContext"
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/articles" element={<ArticlesPage />} />
-        <Route path="/articles/:article_id" element={<IndividualArticle />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <UserProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/articles" element={<ArticlesPage />} />
+          <Route path="/articles/:article_id" element={<IndividualArticle />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </UserProvider>
   )
 }
 
